36300492e53216a6fb3a61f4a2cbb476e398956b
mo.samy.hussein@gmail.com <mo.samy.hussein@gmail.com>
2022-08-03 17:47:27 +0200

    tests returned values by the service

--- test/news_change_notifier_test.dart
+++ test/news_change_notifier_test.dart
@@ -39,10 +39,38 @@ void main() {
   );
 
   group('getArticlea', () {
-    test('get articles using the NewsServices', () async {
-      when(() => mockNewsService.getArticles()).thenAnswer((_) async => []);
-      await sut.getArticles();
-      verify(() => mockNewsService.getArticles()).called(1);
-    });
+    final articlesFromService = [
+      Article(title: 'Test 1', content: 'Test 1 content'),
+      Article(title: 'Test 2', content: 'Test 2 content'),
+      Article(title: 'Test 3', content: 'Test 3 content'),
+    ];
+    void arrangeNewsServiceReturns3Articles() {
+      when(() => mockNewsService.getArticles()).thenAnswer(
+        (_) async => articlesFromService,
+      );
+    }
+
+    test(
+      'get articles using the NewsServices',
+      () async {
+        arrangeNewsServiceReturns3Articles();
+        await sut.getArticles();
+        verify(() => mockNewsService.getArticles()).called(1);
+      },
+    );
+
+    test(
+      '''indicates loading of data,
+      sets articles to the ones from the service,
+      indicates that data is not being loaded anymore''',
+      () async {
+        arrangeNewsServiceReturns3Articles();
+        final future = sut.getArticles();
+        expect(sut.isLoading, true);
+        await future;
+        expect(sut.articles, articlesFromService);
+        expect(sut.isLoading, false);
+      },
+    );
   });
 }
